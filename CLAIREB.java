9cd2e4ae70ee1fd5904ecf19901ca2e70bac0033
boholclaire <clairebohol95@gmail.com>
2024-09-20 07:40:32 +0800

    Add files via upload

---
+++ CLAIREB.java
@@ -0,0 +1,93 @@
+
+package claireb;
+
+import java.util.Scanner;
+
+public class CLAIREB {
+
+ 
+    public static void main(String[] args) {
+         
+     // Scanner sc = new Scanner(System.in);
+     // GRADES[] gr = new GRADES [100];
+    // System.out.println("Enter no. of students: ");
+    //    int nums = sc.nextInt();
+    //  for (int i=0; i< nums; i++){
+     //        System.out.println("Enter details of student "+(i+1)+":");
+     //        System.out.print("ID:");
+     //        int id = sc.nextInt();
+     //        System.out.print("Name:");
+     //        String name = sc.next();
+     //        System.out.print("Prelim:");
+      //       double pr = sc.nextDouble();
+      //       System.out.print("Midterm:");
+       //      double md = sc.nextDouble();
+      //       System.out.print("Prefinal:");
+      //       double pf = sc.nextDouble();
+      //       System.out.print("Final:");
+       //      double fn = sc.nextDouble();
+       //      gr[i] = new GRADES();
+      //       gr[i].addGrades(id, name, pr, md, pf, fn); }
+      //   for(int i=0; i< nums; i++){
+     //    gr[i].viewGrades();
+        
+        
+     
+                   
+     // GRADES gr = new GRADES();
+      // gr.addGrades(1011,"Claire", 1.2, 1.4,1.3, 1.2);
+     //  gr.viewGrades();
+        
+   //   NETPAY np = new NETPAY();
+   //  np.getPay();
+      
+       PRODUCT pr = new PRODUCT();
+       pr.addProduct(10011,"soap", 20, 30, 20);
+       pr.viewProduct();
+     
+     
+     // SALARY Salary = new SALARY();
+     // Salary.addsalaries(101, " Claire", 20.0, 20,100.0);
+     // Salary.getsalaries();
+
+     //  SALARIES Salaries = new SALARIES();
+     //   Salaries.addsalaries(101, "Claire", 60.0, 35,150.0);
+     //  Salaries.getsalaries();
+
+     
+  // Scanner sc = new Scanner(System.in);
+     //   ACCOUNTS accounts = new ACCOUNTS(1, "firstname", "lastname", "email", "username", "password");
+     //   accounts.displayAccountInfo();
+
+      //  System.out.println("Enter new id: ");
+     //   int id = sc.nextInt(); 
+       // sc.nextLine(); 
+
+      //  System.out.println("Enter first name: ");
+      //  String firstName = sc.nextLine(); 
+
+      //  System.out.println("Enter last name: ");
+     //   String lastName = sc.nextLine(); 
+
+    //    System.out.println("Enter email: ");
+     //   String email = sc.nextLine(); 
+
+      //  System.out.println("Enter username: ");
+     //   String username = sc.nextLine();
+
+      //  System.out.println("Enter password: ");
+      //  String password = sc.nextLine();
+
+    
+
+    
+    
+        }
+}
+
+
+    
+
+    
+    
+
